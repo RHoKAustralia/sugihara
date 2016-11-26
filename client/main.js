16192a5fef58711c513afd54deec6701dcee38b7
gitaryaman <apan45@student.monash.edu>
2016-11-26 18:24:14 +1100

    Successfully converts to JSON string, also successfully parses the string. The code is very messy, and needs to be cleaned up. The commented code causes an error, and we don't yet know why.
    Signed-off-by: gitaryaman <apan45@student.monash.edu>

--- client/main.js
+++ client/main.js
@@ -3,12 +3,23 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+var stringified;
+
 Template.main.onCreated(function mainOnCreated() {
-   this.counter = new ReactiveVar(0);
-   this.message = new ReactiveVar("");
-   this.map;
+     this.counter = new ReactiveVar(0);
+     this.message = new ReactiveVar("");
+     this.map;
+    
+    
 });
 
+Template.test.onCreated(function mainOnCreated() {
+     this.counter = new ReactiveVar(0);
+     this.message = new ReactiveVar("");
+     this.map;
+    
+    
+});
 
 Template.main.onRendered(function mainOnRendered()
 {
@@ -68,36 +79,139 @@ Template.main.helpers({
   },
 });
 
+Template.test.onRendered(function mainOnRendered()
+{
+   if (Meteor.isClient)
+   {
+      L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
+
+      var bgn = [-37.802470, 144.966725];
+      var map = Template.instance().map;
+      var opt = { doubleClickZoom: true,
+                  zoomControl: false,
+                  touchZoom: true,
+                  scrollWheelZoom: true,
+                  maxZoom: 20 }
+      map = L.map('map', opt)
+         .setView(bgn, 13);
+
+      L.tileLayer.provider('Thunderforest.Transport').addTo(map);
+
+      // var bgnMsg = "<b>Hello!</b><br>Search a location above.<br>Pinpoint your current location with the button below.";
+      // var marker = L.marker(bgn);
+      // marker.addTo(map);
+      // marker.bindPopup(bgnMsg).openPopup();
+
+      L.control.zoom().setPosition('bottomright').addTo(map);
+
+      var popup = L.popup();
+
+      map.on('click', function(e){
+         popup
+             .setLatLng(e.latlng)
+             .setContent("This location is:<br>" + e.latlng.toString())
+             .openOn(map);
+      });
+
+      var circle = L.circle([-37.78998, 144.93662], 500, {
+         color: 'red',
+         fillColor: '#f03',
+         fillOpacity: 0.5
+      });
+      circle.addTo(map);
+      circle.bindPopup("I am a circle.");
+
+
+      Template.instance().message.set("Map Loaded");
+      Template.instance().map = map;
+   }
+
+});
+
+Template.test.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+  message() {
+    return Template.instance().message.get();
+  },
+});
 
 
 
 
 
-Template.main.events({
+
+/*Template.main.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
     instance.message.set("you pressed the html button");
+  },*/
+      
+Template.main.events({
+  'click #submit-button'(event, instance) {
+      // This code runs when submit is clicked
+      console.log("Submit clicked!");
+      
+      var linkRef = document.getElementById("link");
+      var commentsRef = document.getElementById("comments");
+      
+      var nameRef = document.getElementById("name");
+      var postcodeRef = document.getElementById("postcode");
+      var emailRef = document.getElementById("email");
+      var contactRef = document.getElementById("contact");
+    
+      var formData = {
+          link: linkRef.value,
+          comments: commentsRef.value,
+          name: nameRef.value,
+          postcode: postcodeRef.value,
+          email: emailRef.value,
+          contact: contactRef.value
+      };
+      
+      stringified = JSON.stringify(formData);
+      console.log(stringified);
   },
+      
   "click [data-action='polka']"(event, instance) {
     instance.counter.set(instance.counter.get() + 1);
     instance.message.set("you pressed the nice button");
   },
-  "click [data-action='quack']"(event, instance) {
+});
 
-     var opt = { setView: true,
-                 maxZoom: 16 };
-     instance.map.locate(opt);
-
-     instance.map.on("locationfound", function(e){
-
-        var marker = L.marker(e.latlng);
-        marker.addTo(instance.map);
-        marker.bindPopup("This is your location").openPopup();
-
-        instance.counter.set(999);
-        instance.message.set(e.latlng);
-     });
-
+Template.test.events({
+  'click #submit-button-test'(event, instance) {
+      // This code runs when submit is clicked
+      console.log("Submit clicked!");
+      
+      var linkRefTest = document.getElementById("link-test");
+      console.log(linkRefTest);
+      var commentsRefTest = document.getElementById("comments-test");
+      
+      var nameRefTest = document.getElementById("name-test");
+      var postcodeRefTest = document.getElementById("postcode-test");
+      var emailRefTest = document.getElementById("email-test");
+      var contactRefTest = document.getElementById("contact-test");
+      
+      var formData = JSON.parse(stringified);
+    
+      console.log(formData);
+      
+      /*
+      var linkRefTest.value = formData.link;
+      var commentsRefTest.value = formData.comments;
+      
+      var nameRefTest.value = formData.name;
+      var postcodeRefTest.value = formData.postcode;
+      var emailRefTest.value = formData.email;
+      var contactRefTest.value = formData.contact;
+      */
+  },
+      
+  "click [data-action='polka']"(event, instance) {
+    instance.counter.set(instance.counter.get() + 1);
+    instance.message.set("you pressed the nice button");
   },
 });
