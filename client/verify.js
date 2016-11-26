32657ff2d2e3e59632aca02709659e54d071242a
gitaryaman <apan45@student.monash.edu>
2016-11-26 20:01:26 +1100

    Starting the verify page.
    Signed-off-by: gitaryaman <apan45@student.monash.edu>

---
+++ client/verify.js
@@ -0,0 +1,41 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+var stringified;
+
+Template.main.onCreated(function mainOnCreated() {
+     this.counter = new ReactiveVar(0);
+     this.message = new ReactiveVar("");
+     this.map;
+    
+    
+});
+
+Template.main.onRendered(function mainOnRendered()
+{
+   if (Meteor.isClient)
+   {
+      L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
+   }
+
+});
+
+Template.main.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+  message() {
+    return Template.instance().message.get();
+  },
+});
+
+/*Template.main.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+    instance.message.set("you pressed the html button");
+  },*/
+      
+Template.main.events({});
